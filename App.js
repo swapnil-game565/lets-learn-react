dd600382afa58430e8c2594e4b891a1e4ae80d44
Swapnil Malap <swapnil.malap@neoquant.com>
2025-08-05 18:41:57 +0530

    Learned Javascript Code inside Component

--- App.js
+++ App.js
@@ -1,26 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-//React Functional Component
-const Title = () => (<h1 id="heading" className="head"> 
+//React Element
+const title = (<h1 id="heading" className="head"> 
     This is Title Functional Component
 </h1>);
 
-//Nested React Components also called as Component Composition 
-const Title2 = () => (
-    <div className="second-component">
-        <Title/>
-        <h2> This is second Functional Components and Nested First components in this components</h2>
-    </div>
-)
+//Javascript inside Component
+const sum = 10 * 100;
 
 const Heading = () => (
-     <div id="container">
-        <Title2 /> 
-        <h1 className="heading">Second Commponents nested in This Functional Compoonant</h1>
-     </div>
-);
-
+    <div className="second-component">
+        <h1>{sum}</h1>
+        <h2> Printed Javascript value inside Functional Component. </h2>
+    </div>
+)
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
