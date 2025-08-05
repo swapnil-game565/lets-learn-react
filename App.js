964fc2ea02112533e5eb8cc77c5fc0fde67f369f
Swapnil Malap <swapnil.malap@neoquant.com>
2025-08-05 13:29:16 +0530

    brush up the core react

--- App.js
+++ App.js
@@ -1,40 +1,8 @@
-/**
- * <div id="parent">
- *      <div id="child">
- *          <h1>I'm H1 tag</h1>
- *          <h2>I'm H1 tag</h2>
- *      </div>
- * </div>
- * 
- */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-// this is React Core
-
-// Chatpter 1-3
-    // const heading = React.createElement("h1", //html tag
-    //     { id: "heading" },  
-    //     "Hello world from React!"); 
-    //     //{} , "helloword " are both are props of H1 element.
-
-    //     console.log(heading);  //heading is not a html tag, Its a react object. React liabrary will convert it into html tag.
-
-    // const root = ReactDOM.createRoot(document.getElementById("root")); // this is react DOM
-
-
-    // root.render(heading); // Rending heading into the Root Tag
-
-
-// Chapter 4:
-const parent = React.createElement("div",
-            { id: "parent"},
-                React.createElement("div",
-                    { id:"child"},
-                    [React.createElement("h1",{id:"heading1"},"I'm an H1 tag."),
-                        React.createElement("h2",{id:"heading2"},"I'm an H2 tag.")
-                    ]
-                )
-        );
+//React Element
+const heading = React.createElement("h1",{id:"heading"},"Laying the Foundation");
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(parent);
+root.render(heading);
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # Lets Start The Learing React.
+
+
+# Parcel
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File watching algorithm - written in c++
+- Catching Faster builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Spliting
+- Differntial Bundling - to support older browsers
+- Diagnostic
+- Error Handling
+- Https
+- Tree shaking - remove unused code for you.
+- Different dev and prod bundles
+
