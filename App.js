86fe059989b1d994b32f2ff2bb0b1e1eb1f0ff86
Swapnil Malap <swapnil.malap@neoquant.com>
2025-08-05 19:59:37 +0530

    Learned the how called js, react element and different ways of calling one component inside another

--- App.js
+++ App.js
@@ -2,17 +2,21 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 //React Element
-const title = (<h1 id="heading" className="head"> 
+const Title = () => (<h1 id="heading" className="head"> 
     This is Title Functional Component
 </h1>);
 
+const reactElementObj = <h2> This is react element</h2>;
 //Javascript inside Component
 const sum = 10 * 100;
 
 const Heading = () => (
     <div className="second-component">
         <h1>{100+300} {console.log('this is testing console log inside component.')}</h1>
-        {title}
+        {Title()}
+        <Title />
+        <Title></Title>
+        {reactElementObj}
         <h2> Printed Javascript value inside Functional Component. </h2>
     </div>
 )
